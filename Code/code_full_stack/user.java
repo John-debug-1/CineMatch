fdb93dcea29ed5121f2cceb46c1da777bd267073
mparak07 <thanosmparaklis@gmail.com>
2025-12-04 16:51:55 +0200

    Fix formatting of getPassword method

--- Code/code_full_stack/user.java
+++ Code/code_full_stack/user.java
@@ -19,6 +19,6 @@ public class User {
     public String getUsername() { return username; }
     public void setUsername(String username) { this.username = username; }
 
-    public String getPassword() { return password; }
+    public String getPassword() { return password;  }
     public void setPassword(String password) { this.password = password; }
 }
