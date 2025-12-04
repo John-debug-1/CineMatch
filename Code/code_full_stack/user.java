766979a89ea33f35930b2e051e5b204b9cb3146d
mparak07 <thanosmparaklis@gmail.com>
2025-12-04 16:52:10 +0200

    Fix formatting of getUsername method

--- Code/code_full_stack/user.java
+++ Code/code_full_stack/user.java
@@ -16,7 +16,7 @@ public class User {
     public int getId() { return id; }
     public void setId(int id) { this.id = id; }
 
-    public String getUsername() { return username; }
+    public String getUsername() { return username;}
     public void setUsername(String username) { this.username = username; }
 
     public String getPassword() { return password;  }
